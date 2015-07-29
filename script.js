f8010c018b957f909de1f9aab9199b8badc36a4c
nucular <nucular@users.noreply.github.com>
2015-07-29 16:11:13 +0200

    Added error thing to demo

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 var html = document.getElementById("html");
 var render = document.getElementById("render");
 var toggle = document.getElementById("toggle");
+var error = document.getElementById("error");
 
 var code = CodeMirror.fromTextArea(document.getElementById("code"), {
   mode: "javascript"
 });
 
 function update() {
-  var element = new Function(
-    "with(xmgen.svg) { with (xmgen.html) { return "
-    + code.getValue()
-    + "}}"
-  )();
+  try {
+    var element = new Function(
+      "with(xmgen.svg) { with (xmgen.html) { return "
+      + code.getValue()
+      + "}}"
+    )();
+    error.innerText = "";
+  } catch (e) {
+    error.innerText = e.toString();
+  }
   if (toggle.classList.contains("down")) {
     html.innerText = element.toString(2);
     CodeMirror.colorize([html], "xml");
