e9c40f5acbbb8749078f5ef4ac0a8e9d49fa547e
nucular <nucular@users.noreply.github.com>
2015-08-04 23:40:29 +0200

    Moved stuff around; Removed .prototype but added .constructor

--- src/xmgen.js
+++ src/xmgen.js
@@ -5,7 +5,7 @@
     // Children
     var self = function() {
       var process = function(element) {
-        if (element.prototype == Element.prototype) {
+        if (element.constructor == Element) {
           self._children.push(element);
         } else if (element.constructor == Function) {
           var r = element.bind(self)(process);
@@ -45,58 +45,55 @@
         }
       }
     }
-    self.prototype = Element.prototype;
-    self.toString = Element.prototype.toString;
-    return self;
-  }
-
-  Element.prototype.toString = function(indention, level) {
-    var nl = (indention != undefined) ? "\n" : "";
-    var level = this._type ? (level || 0) : (level || 0)-1;
-    var id = (indention * level > 0) ? new Array(indention * level + 1).join(" ") : "";
-    var string = "";
-    if (this._type) {
-      var isdoctype = this._type.match(/^\!/);
-      var isschema = this._type.match(/^\?/);
-      string += id + "<" + String(this._type);
-      // Attributes
-      for (var k in this) {
-        if (this.hasOwnProperty(k) && !this.prototype.hasOwnProperty(k) && !k.match(/^_/)) {
-          if (this[k] != undefined)
-            string += " " + k + "=\"" + String(this[k]).replace(/"/g, "&quot;") + "\"";
-          else if (this[k] == null)
-            string += " " + k;
+    self.constructor = Element;
+    self.toString = function(indention, level) {
+      var nl = (indention != undefined) ? "\n" : "";
+      var level = this._type ? (level || 0) : (level || 0)-1;
+      var id = (indention * level > 0) ? new Array(indention * level + 1).join(" ") : "";
+      var string = "";
+      if (this._type) {
+        var isdoctype = this._type.match(/^\!/);
+        var isschema = this._type.match(/^\?/);
+        string += id + "<" + String(this._type);
+        // Attributes
+        for (var k in this) {
+          if (this.hasOwnProperty(k) && !(k == "toString" || k == "constructor" || k.match(/^_/))) {
+            if (this[k] != undefined)
+              string += " " + k + "=\"" + String(this[k]).replace(/"/g, "&quot;") + "\"";
+            else if (this[k] == null)
+              string += " " + k;
+          }
+        }
+        if (!isdoctype) {
+          if (this._children.length > 0)
+            string += ">";
+          else if (isschema)
+            string += "?>"
+          else
+            string += "/>";
+        } else {
+          string += " ";
         }
       }
-      if (!isdoctype) {
-        if (this._children.length > 0)
+      // Children
+      for (var i = 0; i < this._children.length; i++) {
+        var child = this._children[i];
+        if (child.constructor == String) {
+          string += child;
+        } else {
+          string += nl + this._children[i].toString(indention, level + 1);
+          if (i == this._children.length - 1) string += nl + id;
+        }
+      }
+      if (this._type) {
+        if (this._children.length > 0 && !isdoctype && !isschema)
+          string += "</" + String(this._type) + ">";
+        if (isdoctype)
           string += ">";
-        else if (isschema)
-          string += "?>"
-        else
-          string += "/>";
-      } else {
-        string += " ";
       }
+      return string;
     }
-    // Children
-    for (var i = 0; i < this._children.length; i++) {
-      var child = this._children[i];
-      if (child.constructor == String) {
-        string += child;
-      } else {
-        string += nl + this._children[i].toString(indention, level + 1);
-        if (i == this._children.length - 1) string += nl + id;
-      }
-    }
-    if (this._type) {
-      if (this._children.length > 0 && !isdoctype && !isschema)
-        string += "</" + String(this._type) + ">";
-      if (isdoctype)
-        string += ">";
-    }
-    return string;
+    return self;
   }
-
   ns.xmgen.Element = Element;
 })(window);
